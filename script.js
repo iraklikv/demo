65302ddd8919be90baf534d694ead3badfa5ac95
iraklikv <iraklikvaratskhelia97@gmail.com>
2023-01-15 20:55:03 +0400

    added script.js file

---
+++ script.js
@@ -0,0 +1,41 @@
+// primitive - 7
+
+// string
+let firstName = 'irakli';
+let lastName = "kvaratskhelia";
+
+console.log(firstName, lastName);
+console.log(lastName);
+console.log(firstName, typeof firstName, lastName, typeof lastName);
+
+firstName = 'giorgi';
+console.log(typeof firstName);
+
+// number
+
+let order = 1.23;
+let age = 25;
+
+console.log(typeof order);
+
+// boolean - true false
+
+let javascriptIsHard = false;
+console.log(typeof javascriptIsHard);
+
+// undefined
+
+let fullName;
+
+console.log(fullName);
+console.log(typeof fullName);
+
+fullName = 'irakli kv';
+
+// null
+
+let emptyValue = null;
+
+
+console.log(emptyValue);
+console.log(typeof emptyValue);
