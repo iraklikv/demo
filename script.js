972fb13b05f5788cfbc662a1691d7f82d0cb12bd
iraklikv <iraklikvaratskhelia97@gmail.com>
2023-01-22 15:06:09 +0400

    test

--- script.js
+++ script.js
@@ -1,41 +1,140 @@
 // primitive - 7
 
 // string
-let firstName = 'irakli';
-let lastName = "kvaratskhelia";
+// let firstName = 'irakli';
+// let lastName = "kvaratskhelia";
 
-console.log(firstName, lastName);
-console.log(lastName);
-console.log(firstName, typeof firstName, lastName, typeof lastName);
+// console.log(firstName, lastName);
+// console.log(lastName);
+// console.log(firstName, typeof firstName, lastName, typeof lastName);
 
-firstName = 'giorgi';
-console.log(typeof firstName);
+// firstName = 'giorgi';
+// console.log(typeof firstName);
 
-// number
+// // number
 
-let order = 1.23;
-let age = 25;
+// let order = 1.23;
+// let age = 25;
 
-console.log(typeof order);
+// console.log(typeof order);
 
-// boolean - true false
+// // boolean - true false
 
-let javascriptIsHard = false;
-console.log(typeof javascriptIsHard);
+// let javascriptIsHard = false;
+// console.log(typeof javascriptIsHard);
 
-// undefined
+// // undefined
 
-let fullName;
+// let fullName;
 
-console.log(fullName);
-console.log(typeof fullName);
+// console.log(fullName);
+// console.log(typeof fullName);
 
-fullName = 'irakli kv';
+// fullName = 'irakli kv';
 
-// null
+// // null
 
-let emptyValue = null;
+// let emptyValue = null;
+
+// console.log(emptyValue);
+// console.log(typeof emptyValue);
+
+// Basic Operators
+
+// + - / * % ** > >= < <= 
+
+const thisYear = 2022;
+
+const irakliAge = thisYear - 1997;
+const mariamiAge = thisYear - 2004;
+
+// console.log(mariamiAge >= 18);
+// console.log(18 <= 18);
+
+// console.log(2023 - 1997);
+
+// console.log(21 % 3);
+
+// console.log(4 % 2);
+
+// console.log(3 ** 10);
+
+// console.log(fullName);
+
+// let x = 10;
+
+// console.log(x);
+
+// x = x * 3;
+
+// x += 1;
+// x++;
+// ++x;
+
+// console.log(++x);
+// console.log(x);
+
+// const y = ++x;
+
+// console.log('y', y);
+// console.log('x', x);
+
+// console.log(5 * 5 > 4 * 6); // 36, 12
+
+// let x = 10 + 5;
+
+// x += 15;
+
+// string literal
+
+// const firstName = `irakli`;
+// const lastName = 'kvaratskhelia';
+// const age = 25;
 
 
-console.log(emptyValue);
-console.log(typeof emptyValue);
+// const fullName = "Hello, my name is " + firstName + ", I'm " + age + ' years old';
+// const newSintax = `Hello, my name is ${firstName} I'm ${25} years old`;
+// console.log(fullName);
+
+// console.log('Multi \n\
+// line \n\
+// string');
+
+// console.log(`Multi
+// line
+// string`);
+
+// if else statement
+
+// let block scoped
+
+
+// თვრამეტზე მაღალი ან დაბალი
+
+// if (age >= 18) {
+//     console.log('above eigtheen');
+// } else {
+//     console.log('below eighteen');
+// }
+
+
+// თვრამეტზე დაბალი, ოცდაერთზე დაბალი ან მაღალი
+
+// const age = 25;
+
+// if (age < 18) {
+//     console.log('below eighteen');
+//     if (age < 10) {
+//         console.log('less than 10');
+//     }
+// } else {
+//     console.log('above twentyone');
+// }  
+
+// console.log('end of if statement');
+
+// type conversion and coercion
+
+// const input = '20';
+// console.log(input, typeof input);
+// console.log(20 + input);
